92090fa4a9b53b440e3b8e1e4d802893a1c4d09f
aaaasif <alexasif4@gmail.com>
2021-10-23 23:54:31 +0600

    modemon added

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require ('express');
 const app = express();
+const port = 5000;
+
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello From nodeJS')
+});
+
+app.listen(port, () => {
+    console.log('listeing to port', port);
+});
