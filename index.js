500c74c27634afb16f59e83debedd5e410d4fc7b
aaaasif <alexasif4@gmail.com>
2021-10-24 20:59:35 +0600

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require ('express');
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const port = 5000;
 
-
-
-app.get('/', (req, res) =>{
-    res.send("Hello From nodeJS. Let's expolore this")
+app.get('/', (req, res) => {
+    res.send('WOW. I am Excited to learn Node and express with nodemon. automatic restart');
 });
 
-const users =[
-    {id:1, name:'Sabana', email:'sabana@gmail.com', pgone: '01798626997' },
-    {id:2, name:'kopila', email:'kopila@gmail.com', pgone: '01798626997' },
-    {id:3, name:'sabnur', email:'sabnur@gmail.com', pgone: '01798626997' },
-    {id:4, name:'porimoni', email:'pormioni@gmail.com', pgone: '01798626997' },
-    {id:5, name:'mousumi', email:'mousumi@gmail.com', pgone: '01798626997' },
-    {id:6, name:'alena', email:'alena@gmail.com', pgone: '01798626997' }
+const users = [
+    { id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888' },
+    { id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'Shrabonti', email: 'Shrabonti@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'Susmita', email: 'Susmita@gmail.com', phone: '01788888888' },
 ]
 
 app.get('/users', (req, res) => {
@@ -28,6 +31,24 @@ app.get('/users', (req, res) => {
         res.send(users)
     }
 });
+
+// app.METHOD
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('hitting the post', req.body)
+    // res.send(JSON.stringify(newUser))
+    res.json(newUser)
+})
+
+// dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id]
+    res.send(user);
+})
+
 app.get('/fruits', (req, res) => {
     res.send(['mango', 'oranges', 'banana', 'apple'])
 })
@@ -37,5 +58,5 @@ app.get('/fruits/mangoes/fazli', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('listeing to port', port);
-});
+    console.log('Listening to port ', port);
+})
