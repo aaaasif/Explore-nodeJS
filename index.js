8b049ff3e0220f6f671ad677ebccc37493781523
aaaasif <alexasif4@gmail.com>
2021-10-24 15:29:12 +0600

    manny file added

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const port = 5000;
 
 
 app.get('/', (req, res) =>{
-    res.send('Hello From nodeJS')
+    res.send("Hello From nodeJS. Let's expolore this")
 });
+
+const users =[
+    {id:1, name:'Sabana', email:'sabana@gmail.com', pgone: '01798626997' },
+    {id:2, name:'kopila', email:'kopila@gmail.com', pgone: '01798626997' },
+    {id:3, name:'sabnur', email:'sabnur@gmail.com', pgone: '01798626997' },
+    {id:4, name:'porimoni', email:'pormioni@gmail.com', pgone: '01798626997' },
+    {id:5, name:'mousumi', email:'mousumi@gmail.com', pgone: '01798626997' },
+    {id:6, name:'alena', email:'alena@gmail.com', pgone: '01798626997' }
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id]
+    res.send(user)
+})
 
 app.listen(port, () => {
     console.log('listeing to port', port);
